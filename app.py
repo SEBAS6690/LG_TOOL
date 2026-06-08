6c7e7a91fd4fcab0320de4e4408f5e128c56c52c
Victor Yanez <victor.yanezm90@gmail.com>
2026-06-08 02:01:53 -0500

    Direccionado a mi propia base de datos de Google Sheets

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def cargar_inventario_dinamico():
                     f"⚠️ **Punto 4:** {p4}",
                     f"⚠️ **Punto 5:** {p5}",
                     f"⚠️ **Punto 5:** {p6}",
-                    f"⚠️ **Punto 5:** {p7}",
+                    f"⚠️ **Punto 5:** {p7}"
                 ]
             }
         return db_dinamica
